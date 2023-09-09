786b7d989c58e4eb1885f76198c79eda207ba0ec
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-09-09 17:32:19 -0600

    Documentacion y mostrar cara detectada agregado

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# Imports necesarios
+# Se importan las utilidades de flask para el sitio web
+# Se importa tensorflow hub para descargar MobileNetV2, se importa CV2 para poder reconocer las caras de las imagenes
+# Se utiliza lbpcascade_animeface para reconocer las caras de animé con cv2
+# Se importa tensorflow y keras para cargar el modelo ya entrenado
+
 from flask import Flask, render_template, request, send_from_directory, flash
 import os
 import tensorflow as tf
@@ -5,34 +11,41 @@ import cv2
 import sys
 from glob import glob
 import itertools
-import matplotlib.pylab as plt
 import numpy as np
 import tensorflow as tf
 import tensorflow_hub as hub
 
-# Especifica la ruta donde esta el modelo
+# Se especifica la ruta donde esta guardado el modelo
 ruta_guardado = "modelo_guardado"
 
-# Carga el modelo
+# Se carga el modelo
 modelo_cargado = tf.keras.models.load_model(ruta_guardado)
 
+# Se configura la aplicacion de Flask
 app = Flask(__name__)
 
-# Configuración para subir archivos
+# Configuración para subir solamente archivos que sean imagenes
 app.config['UPLOAD_FOLDER'] = 'static/images'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 app.config['SECRET_KEY'] = 'aaaaaaasdasdasdasdsadaaaaaaaaasdasdasdasdasa'
 
-# Función para verificar extensiones permitidas
+# Funcion para verificar extensiones permitidas
 def allowed_file(filename):
+    # Esta funcion toma un nombre de archivo y verifica que su extension sea png, jpg, jpeg, gif o webp
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
+# Esta ruta es la pagina principal, aqui se suben las imagenes
 def index():
     return render_template('upload.html')
 
 def detectar_cara(filename, cascade_file = "lbpcascade_animeface.xml"):
+    # Esta funcion utiliza lbpcascade_animeface como archivo de cascade para poder detectar especificamente caras de anime en las imagenes
+    # Toma el filename de la imagen como parametro
+    # Intenta detectar una cara, si no la detecta retorna False
+    # Si detecta una cara entonces se retorna True
+
     if not os.path.isfile(cascade_file):
         raise RuntimeError("%s: no encontrado" % cascade_file)
 
@@ -49,17 +62,24 @@ def detectar_cara(filename, cascade_file = "lbpcascade_animeface.xml"):
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
     
+    # Si se detecto una cara entonces se retorna True y se guarda la imagen como out.png para poder usarla con el modelo para detectar quien es el personaje
     if len(faces) > 0:
         (x, y, w, h) = faces[0]
         face = image[y:y+h, x:x+w]
-        cv2.imshow("AnimeFaceDetect", face)
+        #cv2.imshow("Cara detectada!", face)
         cv2.waitKey(0)
         cv2.imwrite("out.png", face)
+        cv2.imwrite("static/images/out.png", face)
         return True
     else:
         return False
 
 def predecir_personajes(img_path):
+    # Esta funcion calcula los 5 personajes más probables que podría ser una imagen que contiene un personaje de animé
+    # Toma como parametro el filename de una imagen
+    # Usa el modelo para predecir qué personaje podría ser
+    # Retorna el arreglo con los 5 personajes más probables en orden de probabilidad de más probable a menos probable
+
     IMAGE_SIZE = (96,96)
     normalization_layer = tf.keras.layers.Rescaling(1. / 255)
     img = tf.keras.utils.load_img(img_path, target_size=IMAGE_SIZE)
@@ -68,10 +88,6 @@ def predecir_personajes(img_path):
     img_array = normalization_layer(img_array)
 
     class_names = ('Aegis-Persona', 'Ai Hayasaka-Kaguya Sama Love Is War', 'Aisaka Taiga-Toradora', 'Albedo-Overlord', 'Amelia Watson-Hololive', 'Aqua-Konosuba', 'Arcueid Brunestud-Tsukihime', 'Astolfo-Fate', 'Asuka Langley Souryuu-Neon Genesis Evangelion', 'Asuna Yuuki-Sword Art Online', 'C.C-Code Geass', 'Chika Fujiwara-Kaguya Sama Love Is War', 'Cirno-Touhou', 'Emilia-Re Zero', 'Flandre Scarlet-Touhou', 'Fubuki-One Punch Man', 'Gawr Gura-Hololive', 'Giorno Giovanna-JoJos Bizarre Adventure', 'Haruhi Suzumiya-The Melancholy of Haruhi Suzumiya', 'Hatsune Miku-Vocaloid', 'Hifumi Takimoto-New Game!', 'Hinata Hyuuga-Naruto', 'Illyasviel Von Einzbern-Fate', 'Itsuki Nakano-Quintessential Quintuplets', 'Jonathan Joestar-Jojos Bizarre Adventure', 'Kagami Hiiragi-Lucky Star', 'Kaguya Shinomiya-Kaguya Sama Love Is War', 'Kallen Stadtfeld-Code Geass', 'Keqing-Genshin Impact', 'Kirito-Sword Art Online', 'Konata Izumi-Lucky Star', 'Kotobuki Tsumugi-K ON!', 'Kurisu Makise-Steins Gate', 'Lelouch Lamperouge-Code Geass', 'Mai Sakurajima-Rascal Does Not Dream of Bunny Girl Senpai', 'Maka Albarn-Soul Eater', 'Maki Nishikino-Love Live', 'Megumin-Konosuba', 'Miku Nakano-Quintessential Quintuplets', 'Misato Katsuragi-Neon Genesis Evangelion', 'Nami-One Piece', 'Nezuko Kamado-Demon Slayer', 'Nia Teppelin-Gurrenn Lagann', 'Nico Robin-One Piece', 'Nino Nakano-Quintessential Quintuplets', 'Nozomi Tojo-Love Live', 'Ouro Kronii-Hololive', 'Paimon-Genshin Impact', 'Ram-Re Zero', 'Raphtalia-Rising of the Shield Hero', 'Rei Ayanami-Neon Genesis Evangelion', 'Reimu Hakurei-Touhou', 'Rem-Re Zero', 'Rikka Takanashi-Love, Chunibyo and Other Delusions!', 'Rin Tohsakaka-Fate', 'Ritsu Tainaka-K ON!', 'Saber-Fate', 'Sakura Matou-Fate', 'Sakuya Izayoi-Touhou', 'Scarlet Remilia-Touhou', 'Shiki Ryougi-Garden of Sinners', 'Shinji Ikari-Neon Genesis Evangelion', 'Shinobu Kochou-Demon Slayer', 'Shirley Fenette-Code Geass', 'Shouko Komi-Komi Cant Communicate', 'Sinon-Sword Art Online', 'Suzaku Kururugi-Code Geass', 'Yoko Littner-Gurrenn Lagann', 'Yui Hirasawa-K ON!', 'Zero Two-Darling In the Franxx')
-
-    plt.imshow(img_array[0,:,:,:])
-    plt.axis('off')
-    plt.show()
 
     predicciones = modelo_cargado.predict(img_array)
     probabilidades = tf.nn.softmax(predicciones[0])
@@ -93,6 +109,10 @@ def predecir_personajes(img_path):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # Esta ruta sube un archivo de imagen que el usuario seleccionó
+    # Si la imagen contiene una cara entonces se procede a predecir el personaje, si no entonces tira error
+    # Retorna el template de HTML de la pagina de subir archivo
+     
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
@@ -109,18 +129,23 @@ def upload_file():
             resultados = predecir_personajes("out.png")
             return render_template('upload.html', resultados=resultados, filename=file.filename)
         else:
-            flash('Error: No se detectaron caras en la imagen.', 'error')
+            flash('Error: No se detectaron caras. Por favor intenta con otra imagen.', 'error')
             return render_template('upload.html')
     else:
-        flash('Error: Por favor selecciona y sube un archivo.', 'error')
+        flash('Error: No subiste un archivo. Por favor selecciona y sube uno.', 'error')
         return render_template('upload.html')
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
+    # Esta ruta sube un archivo al directorio con las imagenes subidas
+    # Toma como parametro el filename de la imagen por subir
+    # Guarda la imagen en el directorio de imagenes subidas
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/como-usar', methods=['GET','POST'])
 def como_usar():
+    # Esta ruta simplemente carga el HTML de la pagina de tutorial de anidentify
+    # Retorna el HTML
     return render_template('como-usar.html')
 
 if __name__ == '__main__':
