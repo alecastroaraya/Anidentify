4a57095cecc33e68d2cac6f4cabac4a642c0e10c
alejandrocastroarayaitcr <90380164+alecastroitcr@users.noreply.github.com>
2023-09-09 11:29:27 -0600

    Recorte de caras de imagenes agregado
    Ahora al subir una imagen, se detecta la cara y se recorta como parte del preprocesamiento

--- app.py
+++ app.py
@@ -32,20 +32,32 @@ def allowed_file(filename):
 def index():
     return render_template('upload.html')
 
-def build_dataset(subset):
-  # Esta funcion recibe un directorio con clases que contienen imagenes y lo preprocesa para usar como dataset
-  data_dir = "train"
-  IMAGE_SIZE = (96,96)
+def detectar_cara(filename, cascade_file = "lbpcascade_animeface.xml"):
+    if not os.path.isfile(cascade_file):
+        raise RuntimeError("%s: no encontrado" % cascade_file)
 
-  return tf.keras.preprocessing.image_dataset_from_directory(
-      data_dir,
-      validation_split=.18,
-      subset=subset,
-      label_mode="categorical",
-
-      seed=321,
-      image_size=IMAGE_SIZE,
-      batch_size=1)
+    cascade = cv2.CascadeClassifier(cascade_file)
+    image = cv2.imread(filename, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+    
+    faces = cascade.detectMultiScale(gray,
+                                     # opciones de detector de caras
+                                     scaleFactor = 1.1,
+                                     minNeighbors = 5,
+                                     minSize = (24, 24))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    
+    if len(faces) > 0:
+        (x, y, w, h) = faces[0]
+        face = image[y:y+h, x:x+w]
+        cv2.imshow("AnimeFaceDetect", face)
+        cv2.waitKey(0)
+        cv2.imwrite("out.png", face)
+        return True
+    else:
+        return False
 
 def predecir_personajes(img_path):
     IMAGE_SIZE = (96,96)
@@ -55,8 +67,7 @@ def predecir_personajes(img_path):
     img_array = tf.expand_dims(img_array, 0)
     img_array = normalization_layer(img_array)
 
-    train_ds = build_dataset("training")
-    class_names = tuple(train_ds.class_names)
+    class_names = ('Aegis-Persona', 'Ai Hayasaka-Kaguya Sama Love Is War', 'Aisaka Taiga-Toradora', 'Albedo-Overlord', 'Amelia Watson-Hololive', 'Aqua-Konosuba', 'Arcueid Brunestud-Tsukihime', 'Astolfo-Fate', 'Asuka Langley Souryuu-Neon Genesis Evangelion', 'Asuna Yuuki-Sword Art Online', 'C.C-Code Geass', 'Chika Fujiwara-Kaguya Sama Love Is War', 'Cirno-Touhou', 'Emilia-Re Zero', 'Flandre Scarlet-Touhou', 'Fubuki-One Punch Man', 'Gawr Gura-Hololive', 'Giorno Giovanna-JoJos Bizarre Adventure', 'Haruhi Suzumiya-The Melancholy of Haruhi Suzumiya', 'Hatsune Miku-Vocaloid', 'Hifumi Takimoto-New Game!', 'Hinata Hyuuga-Naruto', 'Illyasviel Von Einzbern-Fate', 'Itsuki Nakano-Quintessential Quintuplets', 'Jonathan Joestar-Jojos Bizarre Adventure', 'Kagami Hiiragi-Lucky Star', 'Kaguya Shinomiya-Kaguya Sama Love Is War', 'Kallen Stadtfeld-Code Geass', 'Keqing-Genshin Impact', 'Kirito-Sword Art Online', 'Konata Izumi-Lucky Star', 'Kotobuki Tsumugi-K ON!', 'Kurisu Makise-Steins Gate', 'Lelouch Lamperouge-Code Geass', 'Mai Sakurajima-Rascal Does Not Dream of Bunny Girl Senpai', 'Maka Albarn-Soul Eater', 'Maki Nishikino-Love Live', 'Megumin-Konosuba', 'Miku Nakano-Quintessential Quintuplets', 'Misato Katsuragi-Neon Genesis Evangelion', 'Nami-One Piece', 'Nezuko Kamado-Demon Slayer', 'Nia Teppelin-Gurrenn Lagann', 'Nico Robin-One Piece', 'Nino Nakano-Quintessential Quintuplets', 'Nozomi Tojo-Love Live', 'Ouro Kronii-Hololive', 'Paimon-Genshin Impact', 'Ram-Re Zero', 'Raphtalia-Rising of the Shield Hero', 'Rei Ayanami-Neon Genesis Evangelion', 'Reimu Hakurei-Touhou', 'Rem-Re Zero', 'Rikka Takanashi-Love, Chunibyo and Other Delusions!', 'Rin Tohsakaka-Fate', 'Ritsu Tainaka-K ON!', 'Saber-Fate', 'Sakura Matou-Fate', 'Sakuya Izayoi-Touhou', 'Scarlet Remilia-Touhou', 'Shiki Ryougi-Garden of Sinners', 'Shinji Ikari-Neon Genesis Evangelion', 'Shinobu Kochou-Demon Slayer', 'Shirley Fenette-Code Geass', 'Shouko Komi-Komi Cant Communicate', 'Sinon-Sword Art Online', 'Suzaku Kururugi-Code Geass', 'Yoko Littner-Gurrenn Lagann', 'Yui Hirasawa-K ON!', 'Zero Two-Darling In the Franxx')
 
     plt.imshow(img_array[0,:,:,:])
     plt.axis('off')
@@ -92,9 +103,14 @@ def upload_file():
 
         # Carga el modelo
         modelo_cargado = tf.keras.models.load_model(ruta_guardado)
-        resultados = predecir_personajes(filename)
 
-        return render_template('upload.html', resultados=resultados, filename=file.filename)
+        cara_encontrada = detectar_cara(filename)
+        if cara_encontrada:
+            resultados = predecir_personajes("out.png")
+            return render_template('upload.html', resultados=resultados, filename=file.filename)
+        else:
+            flash('Error: No se detectaron caras en la imagen.', 'error')
+            return render_template('upload.html')
     else:
         flash('Error: Por favor selecciona y sube un archivo.', 'error')
         return render_template('upload.html')
